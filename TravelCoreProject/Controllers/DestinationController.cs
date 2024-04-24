e3e692ff13d304ee178a029817806180004bc460
Ayşenur Balkan <balkan.aysenur@gmail.com>
2024-04-24 10:50:58 +0300

    Controller

---
+++ TravelCoreProject/Controllers/DestinationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TBusinessLayer.Concrete;
+using TDataAccesLayer.EntityFramework;
+
+namespace TravelCoreProject.Controllers
+{
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetlist();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+            ViewBag.i = id;
+            var values = destinationManager.TGetById(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult DestinationDetails(DestinationController p)
+        {
+            return View();
+        }
+    }
+}
---
+++ TravelCoreProject/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelCoreProject.Models;
+
+namespace TravelCoreProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Index Sayfası Çağırıldı.");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            _logger.LogInformation("Privacy Sayfası Çağırıldı.");
+            _logger.LogError("Error Log Çağırıldı.");
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            DateTime d = Convert.ToDateTime(DateTime.Now.ToLongDateString());
+            _logger.LogInformation(d+"Test Sayfası Çağırıldı.");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
